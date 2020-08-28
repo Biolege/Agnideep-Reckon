5090e87ed35b6279c3836a1d622cf2cf72738a32
Reckon Mazumdar <reckonmazumdar12@gmail.com>
2020-08-28 13:20:38 +0530

    Bookings1

---
+++ src/Components/bookAppointment.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const bookAppointment = () => {
+  return (
+    <div class="row d-sm-none d-block">
+      <div class="col-sm-4 col-12 offset-sm-9">
+        <button
+          type="button"
+          class="btn btn-warning btn-sm px-3 btn-block-xs-only"
+        >
+          <b>Book Appointment</b>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default bookAppointment;
