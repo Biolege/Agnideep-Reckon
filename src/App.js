5338af16f8340336b74a87fe830b8524c3dca090
Reckon Mazumdar <reckonmazumdar12@gmail.com>
2020-08-29 12:09:11 +0530

    appointment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Appointment from "./components/appointment/appointment";
+import Tabs from "./components/tabs/tabs";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Appointment />
+      <div class="row d-sm-none d-block">
+        <div class="col-sm-4 col-12 offset-sm-9">
+          <button
+            type="button"
+            class="btn btn-warning btn-sm px-3 btn-block-xs-only"
+          >
+            <b>Book Appointment</b>
+          </button>
+        </div>
+      </div>
+      <Tabs />
     </div>
   );
 }
